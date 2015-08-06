ade576d9d97e5aed8301329fc1c7dd2572c81f74
Manish Kumar <Manish.Kumar@dealertrack.com>
2015-08-06 15:42:03 +0530

    Enable static

--- whtscooking/whtscooking/settings.py
+++ whtscooking/whtscooking/settings.py
@@ -99,6 +99,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = (
+		    BASE_DIR + '/whtscooking/static',)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
